b9b962682e658465ef745f4d5e490602698e10e8
lwd12 <122261110+lwd12@users.noreply.github.com>
2023-05-17 14:28:25 +0900

    Delete smart_parking directory

--- smart_parking/camera/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'camera'
-
-urlpatterns = [
-    path('', views.camera, name='camera'),
-]
--- smart_parking/config/settings/prod.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-ALLOWED_HOSTS = ['3.34.74.107']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
-DEBUG = False
